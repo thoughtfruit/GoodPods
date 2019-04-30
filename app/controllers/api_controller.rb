7d048b535e4949b9d66af2fd291f68c13b321756
Dain Miller <miller.dain@gmail.com>
2019-04-29 23:02:45 -0700

    Updates for user auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ActionController::Base
 
-  before_action :authenticate
+  #before_action :authenticate
 
   private
   def client_id ; @_client_id ||= (params[:client_id] || request.headers["HTTP_CLIENT_ID"] || NullClientId) ; end
