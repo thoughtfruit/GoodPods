2ddccb75c94370f60e9f08670f6c775bbf361b07
Dain Miller <miller.dain@gmail.com>
2019-04-30 00:29:10 -0700

    Extract some key components for MPA framework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@ self = this;
 
 window.App = {
   rootUrl: 'http://localhost:3000',
-  clientId: 'client_id=078932198103829020278292027898289223828',
+  clientId: '?client_id=078932198103829020278292027898289223828',
 
   initialize: function() {
     this.bootRoutes();
@@ -41,7 +41,7 @@ window.App = {
 
   routes: {
     1: function() {
-      App.checkIfPathIsSet('homepage')
+      App.utils.checkIfPathIsSet('homepage')
       return {
         run: App.pages['homepage']
       }
@@ -54,61 +54,68 @@ window.App = {
       App.sections().updates();
       App.sections().myListening();
     },
-    pageTwo: function() {
+    discussions: function() {
+      App.sections().discussions();
     }
   },
+};
 
-  sections: function() {
-    function _discover() {
-      $el = $('.discover')
+window.App.sections = function() {
+  function _discover() {
+    $el = $('.discover')
 
-      // Fetch & Render abstraction
-      // Fetcher, Renderer
-      fetch().andRender();
+    // Fetch & Render abstraction
+    // Fetcher, Renderer
+    fetch().andRender();
+    // TODO: Add itunes image and stuff for podcasts 
+    // TODO: Add episodes domain model to allow for playing episodes
 
-      function fetch() {
-        $.ajax({
-          // TODO: Extract attributes a model
-          // url: App.rootUrl + App.models.podcasts.url + App.clientId,
-          url: App.rootUrl + '/podcasts?' + App.clientId,
-          success: (data) => {
-            return {
-              render: renderPodcasts(data)
-            }
+    function fetch() {
+      $.ajax({
+        // TODO: Extract attributes to a model
+        // url: App.rootUrl + App.models.podcasts.url + App.clientId,
+        url: App.models['discover']['url'] + App.clientId,
+        success: (data) => {
+          return {
+            andRender: renderPodcasts(data)
           }
-        })
-      }
-
-      function renderPodcasts(data) {
-        $el.html("")
-        data.podcasts.forEach(podcast => {
-          $el.append(
-            "<div>" + podcast.title + "</div>"
-          )
-        })
-      }
-
-    };
-    function _updates() {
-      console.log("Updates hit");
-    };
-    function _myListening() {
-      console.log("myListening hit");
-    };
-
-    return {
-      discover: _discover,
-      updates: _updates,
-      myListening: _myListening
+        }
+      })
     }
-  },
 
-  checkIfPathIsSet: function(path) {
-    if (window.location.pathname.indexOf(path) != -1) {
-      return true;
-    } else {
-      return false;
+    function renderPodcasts(data) {
+      $el.html("")
+      data.podcasts.forEach(podcast => {
+        $el.append(
+          "<div>" + podcast.title + "</div>"
+        )
+      })
     }
+
+  };
+  function _updates() {
+    console.log("Updates hit");
+  };
+  function _myListening() {
+    console.log("myListening hit");
+  };
+
+  return {
+    discover: _discover,
+    updates: _updates,
+    myListening: _myListening
   }
 };
 
+window.App.models = {}
+App.models.discover = {}
+App.models.discover.url = '/podcasts'
+
+window.App.utils = {}
+App.utils.checkIfPathIsSet = function(path) {
+  if (window.location.pathname.indexOf(path) != -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
