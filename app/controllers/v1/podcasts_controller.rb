346f99be069413e4176f80a12e9c1b5cbb824c1b
Dain Miller <miller.dain@gmail.com>
2019-05-02 00:05:11 -0700

    `Update` section improvements - adding initial styling and DOM 

--- app/controllers/v1/podcasts_controller.rb
+++ app/controllers/v1/podcasts_controller.rb
@@ -2,8 +2,8 @@ module V1
   class PodcastsController < ApiController
     def index
       @podcasts = Podcast.all
+      render json: @podcasts
     end
-
     def create
       @podcast = Podcast.create! params
     end
