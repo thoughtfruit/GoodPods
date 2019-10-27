f49a493d71078b06b92037150616ea4c94a640c2
Dain Miller <miller.dain@gmail.com>
2019-10-27 03:00:20 -0400

    Fix for real this time

--- db/migrate/20190518234721_create_statuses.rb
+++
@@ -1,9 +0,0 @@
-class CreateEpisodes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :statuses do |t|
-      t.string :status_name
-      t.belongs_to :podcast, foreign_key: true
-      t.belongs_to :user, foreign_key: true
-    end
-  end
-end
