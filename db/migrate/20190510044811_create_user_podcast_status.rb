ea95de26706c4e9dc919673da353d19fb8f7b8f7
Dain Miller <miller.dain@gmail.com>
2019-10-28 03:34:41 -0400

    Attempt to fix a DB issue in prod

--- db/migrate/20190510044811_create_user_podcast_status.rb
+++ db/migrate/20190510044811_create_user_podcast_status.rb
@@ -3,6 +3,7 @@ class CreateUserPodcastStatus < ActiveRecord::Migration[5.2]
     create_table :user_podcast_statuses do |t|
       t.belongs_to :user, foreign_key: true
       t.belongs_to :podcast, foreign_key: true
+      t.text :status
     end
   end
 end
