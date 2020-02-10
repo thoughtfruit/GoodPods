4335941a81d4a6161a11082e702c6e149fdaa4be
Dain Miller <miller.dain@gmail.com>
2020-02-10 05:46:17 -0500

    Prep feature tests for fixtures

--- spec/features/user_follows_another_user_spec.rb
+++ spec/features/user_follows_another_user_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 feature "User follows another user" do 
 
   scenario "successfully" do
-    user = User.create!
-    user2 = User.create!
     visit user_path(user)
     click_on "Follow"
     expect(page).to have_css('.followed', count: 1, text: "Followed")
