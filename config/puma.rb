61ad3fa2a00fe86806c8453b27873021a8bee285
Dain Miller <miller.dain@gmail.com>
2020-02-11 15:27:55 -0500

    Attempt to get language metrics working

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,11 @@
+require 'barnes'
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-require 'barnes'
 
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
@@ -36,5 +37,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 before_fork do
-  Barnes.start
 end
+
+Barnes.start
