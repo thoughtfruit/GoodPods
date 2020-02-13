c98bb8f43ff8b08396164dcf3d756b98ddbe045d
Dain Miller <miller.dain@gmail.com>
2020-02-13 17:29:07 -0500

    Slight refactor to DiscoveryService to allow iterating over pages in URLs

--- app/services/discovery_service.rb
+++ app/services/discovery_service.rb
@@ -1,18 +1,22 @@
-# TODO Refactor this entire file 
+# TODO Refactor this entire file
 class DiscoveryService
-  def self.start!
-    DiscoverRankedPodcastsAlt.new
+  def self.start
+    DiscoverRankedPodcastsFromChartable.new
   end
 end
 
-class DiscoverRankedPodcastsAlt
+class DiscoverRankedPodcastsFromChartable
 
   def initialize
-    @podcasts = Podcast.all
-    @page = 1
-    urls.each do |url|
-      discover url
+    @pages = (1..10).to_a
+    @pages.each do |page_number|
+      urls(page_number).each do |url|
+        puts "STARTING TO FETCH URL #{url}".red
+        discover url
+      end
     end
+    # Podcast.where(genre: nil).all.each &:destroy
+    # Podcast.where(genre: "").all.each &:destroy
   end
 
   def discover url
@@ -25,28 +29,30 @@ class DiscoverRankedPodcastsAlt
 
         begin
           title_from_chart = podcast[0].strip
-          podcast_ranking = ranking_algo podcast
+          # podcast_ranking = ranking_algo podcast
           podcast = Podcast.find_by(title: title_from_chart)
 
           if podcast
-            podcast.update! ranking: podcast_ranking
+            puts "Already have this pod".yellow
+            # podcast.update! ranking: podcast_ranking
             t = HTTParty.get("https://itunes.apple.com/search?term=#{podcast.title}").body
             t = JSON.parse(t)
             podcast.update! itunes_url: t['results'][0]['trackViewUrl']
             podcast.update! genre: t['results'][0]['genres'][0]
+            podcast.update! logo_url: t['results'][0]['artworkUrl100']
             podcast.update! logo_url_large: t['results'][0]['artworkUrl600']
-            puts t['results'][0]
           else
+            puts "Ready to save new podcast".green
             podcast = Podcast.create!(
               title: title_from_chart,
-              ranking: podcast_ranking,
+              # ranking: podcast_ranking,
               network: Network.last,
               cluster: Cluster.last
             )
 
             t = HTTParty.get("https://itunes.apple.com/search?term=#{podcast.title}").body
             t = JSON.parse(t)
-            podcast.update! logo_url: t['results'][0]['artworkUrl60']
+            podcast.update! logo_url: t['results'][0]['artworkUrl100']
             podcast.update! feed_url: t['results'][0]['feedUrl']
             podcast.update! genre: t['results'][0]['genres'][0]
             podcast.update! logo_url_large: t['results'][0]['artworkUrl600']
@@ -62,9 +68,8 @@ class DiscoverRankedPodcastsAlt
         @page_length -= 1
       rescue
       end
+    else
     end
-    Podcast.where(genre: nil).all.each &:destroy
-    Podcast.where(genre: "").all.each &:destroy
   end
 
   def recurse url
@@ -79,17 +84,17 @@ class DiscoverRankedPodcastsAlt
     a = a * @page
     return a
   end
-  def urls
+  def urls page_number
     [
-      "https://chartable.com/charts/spotify/united-states-of-america-comedy?page=#{@page}",
-      "https://chartable.com/charts/spotify/united-states-of-america-educational?page=#{@page}",
+      "https://chartable.com/charts/spotify/united-states-of-america-comedy?page=#{page_number}",
+      "https://chartable.com/charts/spotify/united-states-of-america-educational?page=#{page_number}",
       "https://chartable.com/charts/spotify/united-states-of-america-top-podcasts",
-      "https://chartable.com/charts/spotify/united-states-of-america-stories?page=#{@page}",
-      "https://chartable.com/charts/itunes/us-all-podcasts-podcasts?page=#{@page}",
-      "https://chartable.com/charts/stitcher/all-most-shared?page=#{@page}",
-      "https://chartable.com/charts/stitcher/all-top-movers?page=#{@page}",
-      "https://chartable.com/charts/stitcher/all-top-shows?page=#{@page}",
-      "https://chartable.com/charts/itunes/us-all-podcasts-podcasts?page=#{@page}"
+      "https://chartable.com/charts/spotify/united-states-of-america-stories?page=#{page_number}",
+      "https://chartable.com/charts/itunes/us-all-podcasts-podcasts?page=#{page_number}",
+      "https://chartable.com/charts/stitcher/all-most-shared?page=#{page_number}",
+      "https://chartable.com/charts/stitcher/all-top-movers?page=#{page_number}",
+      "https://chartable.com/charts/stitcher/all-top-shows?page=#{page_number}",
+      "https://chartable.com/charts/itunes/us-all-podcasts-podcasts?page=#{page_number}"
     ]
   end
 end
